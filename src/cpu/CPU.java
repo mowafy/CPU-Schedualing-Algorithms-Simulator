c84f9e4439dca619ae557cd6dd928c3cf1b7c5b6
Mowafy <eng.ahmed_elmowafy@yahoo.com>
2014-06-09 13:58:23 +0200

    Create CPU.java

---
+++ src/cpu/CPU.java
@@ -0,0 +1,14 @@
+package cpu;
+
+/**
+ * @author Ahmed Elmowafy
+ * Main class of the application that initializes Face Frame.
+ */
+public class CPU {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+            Face face = new Face();
+            face.show();
+    }
+}
